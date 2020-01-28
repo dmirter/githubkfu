9d8130e294233ef2027f451e8886e2123140f92a
NikitaGitHub19 <55347556+NikitaGitHub19@users.noreply.github.com>
2020-01-28 21:37:01 +0300

    Update Task14.cpp

--- Practice/CAPP14/Task14.cpp
+++ Practice/CAPP14/Task14.cpp
@@ -1,16 +1,17 @@
-﻿#include "pch.h"
 #include <iostream>
 using namespace std;
-
 int main()
 {
 	int s, l1, r1, l2, r2;
 	bool flag = false;
 	cin >> s>> l1>> r1>> l2>> r2;
 	int l2_2 = l2;
-	for (; l1 <= r1; l1++) {
-		for (l2=l2_2; l2 <= r2; l2++) {
-			if (l1 + l2 == s) { 
+	for (; l1 <= r1; l1++)//проверка первого диапозона (0-3=0,1,2,3)
+	{
+		for (l2=l2_2; l2 <= r2; l2++)//проверка второго диапозона 
+		{
+			if (l1 + l2 == s)// 
+			{ 
 				flag = true;
 				break;
 			}
