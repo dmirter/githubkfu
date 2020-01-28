3e6a50f7b54173c3dfdd013803b2a92cdac98207
NikitaGitHub19 <55347556+NikitaGitHub19@users.noreply.github.com>
2020-01-28 21:48:35 +0300

    Update Task13.cpp

--- Practice/CAPP13/Task13.cpp
+++ Practice/CAPP13/Task13.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 using namespace std;
-
 int main()
 {
 	int n, x = 1, counter = 0;
 	cin >> n;
-	if (n != 0) {
-		while (x <= n) {
-			counter++;
-			x *= 2;
+	if (n != 0) 
+	{
+		while (x <= n) 
+		{
+			counter++;// кол-во степеней
+			x *= 2;// увеличение степени двойки
 		}
 	}
 	cout << counter << endl;
